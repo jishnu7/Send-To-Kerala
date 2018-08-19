60a6ce8a4920e09d89e1ddf4d6db0451b42b3f7a
Jishnu Mohan <jishnu7@gmail.com>
2018-08-19 12:29:25 +0530

    import css from coc

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 (function() {
   var options = {
     valueNames: [
+      'country',
       'state',
       'city',
       'address',
@@ -14,29 +15,33 @@
       'last-update'
     ],
 
-    item: `<li>
-        <p class="state"></p>
-        <p class="city"></p>
-        <p class="address"></p>
-        <a class="gps" href="gps">Google Maps</a>
-        <p class="next-load"></p>
-        <p class="target-centre"></p>
-        <p class="active-till"></p>
-        <p class="contact"></p>
-        <p class="priority-items"></p>
-        <p class="added-on"></p>
-        <p class="last-update"></p>
-    </li>`
+    item: `<ul>
+        <li class="state"></li>
+        <li class="city"></li>
+        <li class="address"></li>
+        <li>
+          <a class="gps" href="gps">Google Maps</a>
+        </li>
+        <li class="next-load"></li>
+        <li class="target-centre"></li>
+        <li class="active-till"></li>
+        <li class="contact"></li>
+        <li class="priority-items"></li>
+        <li class="added-on"></li>
+        <li class="last-update"></li>
+    </ul>`
   };
   
   var values = [
     {
+      'country': 'India',
       'state': 'Karnataka',
       'city': 'Bangalore',
       'address': 'Opps to Madiwala Ayyappa Temple',
       'gps': 'https://maps.google.com/foobar',
       'next-load': 'Aug 20, 10PM',
       'active-till': 'Aug 20, 9PM',
+      'target-centre': 'Chengannur',
       'contact': '987654321',
       'priority-items': 'Sanitary Napkins',
       'added-on': 'Aug 19, 5AM',
