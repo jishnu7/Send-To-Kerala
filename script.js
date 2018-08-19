5cfc15c5bc8c65d22c12ae52a47dc5583e98339a
Jishnu Mohan <jishnu7@gmail.com>
2018-08-19 12:42:29 +0530

    move out collection centres data

--- script.js
+++ script.js
@@ -32,22 +32,5 @@
     </ul>`
   };
   
-  var values = [
-    {
-      'country': 'India',
-      'state': 'Karnataka',
-      'city': 'Bangalore',
-      'address': 'Opps to Madiwala Ayyappa Temple',
-      'gps': 'https://maps.google.com/foobar',
-      'next-load': 'Aug 20, 10PM',
-      'active-till': 'Aug 20, 9PM',
-      'target-centre': 'Chengannur',
-      'contact': '987654321',
-      'priority-items': 'Sanitary Napkins',
-      'added-on': 'Aug 19, 5AM',
-      'last-update': 'Aug 19, 7AM'
-    }
-  ];
-  
-  var userList = new List('users', options, values);
+  var userList = new List('users', options, COLLECTION_CENTRES);
 })();
