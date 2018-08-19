0c76fc7ae8450acf8a3220b77b8280184c25e60f
Jishnu Mohan <jishnu7@gmail.com>
2018-08-19 18:00:10 +0530

    new key names

--- script.js
+++ script.js
@@ -58,23 +58,26 @@
     </div>`
   };
 
+  function getProp (row, name) {
+    return row['gsx$' + name] &&
+      row['gsx$' + name].$t || '';
+  }
 
   function parseRow (row) {
     return {
-      'country': row['gsx$contact'] && row['gsx$contact'].$t,
-      'state': row['gsx$state'] && row['gsx$state'].$t,
-      'city': row['gsx$city'] && row['gsx$city'].$t,
-      'address': row['gsx$address'] && row['gsx$address'].$t,
-      'gps': row['gsx$gps'] && row['gsx$gps'].$t,
-      'next-load': row['gsx$next-load'] && row['gsx$next-load'].$t,
-      'active-till': row['gsx$active-till'] && row['gsx$active-till'].$t,
-      'target-centre': row['gsx$target-centre'] && row['gsx$target-centre'].$t,
-      'contact': row['gsx$contact'] && row['gsx$contact'].$t,
-      'priority-items': row['gsx$contact'] && row['gsx$contact'].$t,
-      'added-on': row['gsx$timestamp'] && row['gsx$timestamp'].$t,
-      'last-update': row['gsx$last-update'] && row['gsx$last-update'].$t,
-      'status': row['gsx$status'] && row['gsx$status'].$t,
-      'verified': row['gsx$verified'] && row['gsx$verified'].$t
+      'country': getProp(row, 'country'),
+      'state': getProp(row, 'state'),
+      'city': getProp(row, 'location'),
+      'address': getProp(row, 'locationaddress'),
+      'gps': getProp(row, 'addressmaplink'),
+      'next-load': getProp(row, 'nextload'),
+      'active-till': getProp(row, 'centreactivetill'),
+      'target-centre': getProp(row, 'targetcentre'),
+      'contact': getProp(row, 'contactdetails'),
+      'priority-items': getProp(row, 'priorityitems'),
+      'added-on': getProp(row, 'timestamp'),
+      'last-update': getProp(row, 'lastupdate'),
+      'verified': getProp(row, 'verified')
     }
   }
 
